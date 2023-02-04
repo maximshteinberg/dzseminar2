82804e0185ab5b342f145c1e7b53efe84e0e09ed
Maxim Shteinberg <maxim518@mail.ru>
2023-02-04 23:42:09 +0300

    dzseminar2

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<999) 
+{
+  int num1 = (number/10)%10;
+  System.Console.WriteLine("второе число: " + num1);
+}
+else System.Console.WriteLine("Число не трехзначное");
---
+++ task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100) 
+{
+    System.Console.WriteLine("Отсутсвует третье число");
+}
+else
+{
+    while (number > 999)
+{
+    number /= 10;
+}
+System.Console.WriteLine(number%10);
+}
+
+
